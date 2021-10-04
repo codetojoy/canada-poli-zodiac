f2612a00ee6592b9d37d4c556a017388a7a519fc
Michael Easter <codetojoy@gmail.com>
2021-10-04 11:38:12 -0300

    dedicated French JSON

--- viz/server.sh
+++ viz/server.sh
@@ -7,4 +7,9 @@ cp ../zodiac_federal_mp_elements.json .
 cp ../zodiac_federal_mp_provinces.json . 
 cp ../zodiac_federal_mp_unknown.json . 
 
+cp ../zodiac_federal_mp_fr.json . 
+cp ../zodiac_federal_mp_elements_fr.json . 
+cp ../zodiac_federal_mp_provinces_fr.json . 
+cp ../zodiac_federal_mp_unknown_fr.json . 
+
 python3 -m http.server
