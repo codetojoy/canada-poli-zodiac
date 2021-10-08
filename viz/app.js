c66f0fdf156c7ac223813c0823218c5df97bde0f
Michael Easter <codetojoy@gmail.com>
2021-10-08 17:01:26 -0300

    legend and alt colors

--- viz/app.js
+++ viz/app.js
@@ -22,6 +22,20 @@ const BLOC_QUEBECOIS_PARTY = "Bloc Québécois";
 const INDEPENDENT = "Independent";
 const UNKNOWN_PARTY = "Unknown";
 
+const GREEN_PARTY_LEGEND_FR = "Vert";
+const LIBERAL_PARTY_LEGEND_FR = "Libéral";
+const NDP_PARTY_LEGEND_FR = "NPD";
+const CONSERVATIVE_PARTY_LEGEND_FR = "PCC";
+const BLOC_QUEBECOIS_PARTY_LEGEND_FR = "BQ";
+const INDEPENDENT_LEGEND_FR = "Ind.";
+
+const GREEN_PARTY_LEGEND = "Green";
+const LIBERAL_PARTY_LEGEND = "Liberal";
+const NDP_PARTY_LEGEND = "NDP";
+const CONSERVATIVE_PARTY_LEGEND = "CPC";
+const BLOC_QUEBECOIS_PARTY_LEGEND = "BQ";
+const INDEPENDENT_LEGEND = "Ind.";
+
 const NUM_SIBLINGS_FOR_SMALL_TEXT = 4;
 const NUM_SIBLINGS_FOR_TINY_TEXT = 10;
 const NUM_SIBLINGS_FOR_MICRO_TEXT = 16;
@@ -32,6 +46,7 @@ const MODE_NORMAL = "normal";
 const MODE_ELEMENTS = "elements";
 const MODE_PROVINCES = "provinces";
 const MODE_UNKNOWN = "unknown";
+const MODE_ALT_COLORS = "alt-colors";
 
 const MODE_MAP = {
   [MODE_NORMAL]: NORMAL_JSON_FILE,
@@ -45,18 +60,89 @@ const PARTY_COLOR_MAP = {
   [BLOC_QUEBECOIS_PARTY]: d3.rgb(19, 130, 157), // 13 82 9D
   [CONSERVATIVE_PARTY]: d3.rgb(18, 76, 154), // 12 4C 9A
   [GREEN_PARTY]: d3.rgb(50, 134, 43), // 32 86 2B
-  [INDEPENDENT]: d3.rgb(192, 192, 192),
+  [INDEPENDENT]: d3.rgb(103, 103, 103), // 67 67 67
   [LIBERAL_PARTY]: d3.rgb(163, 15, 21), // A3 0F 15
   [NDP_PARTY]: d3.rgb(216, 62, 24), // D8 3E 18
 };
 
+// from: https://medialab.github.io/iwanthue/
+const PARTY_ALT_COLOR_MAP = {
+  [BLOC_QUEBECOIS_PARTY]: d3.rgb(151, 100, 201),
+  [CONSERVATIVE_PARTY]: d3.rgb(18, 76, 154),
+  [GREEN_PARTY]: d3.rgb(101, 159, 96),
+  [INDEPENDENT]: d3.rgb(172, 165, 53),
+  [LIBERAL_PARTY]: d3.rgb(198, 92, 138),
+  [NDP_PARTY]: d3.rgb(197, 100, 69),
+};
+
+const LEGEND_MAP = {
+  [BLOC_QUEBECOIS_PARTY_LEGEND]: { delta: 105, code: [BLOC_QUEBECOIS_PARTY], fr: [BLOC_QUEBECOIS_PARTY_LEGEND_FR] },
+  [CONSERVATIVE_PARTY_LEGEND]: { delta: 105, code: [CONSERVATIVE_PARTY], fr: [CONSERVATIVE_PARTY_LEGEND_FR] },
+  [GREEN_PARTY_LEGEND]: { delta: 105, code: [GREEN_PARTY], fr: [GREEN_PARTY_LEGEND_FR] },
+  [INDEPENDENT_LEGEND]: { delta: 105, code: [INDEPENDENT], fr: [INDEPENDENT_LEGEND_FR] },
+  [LIBERAL_PARTY_LEGEND]: { delta: 105, code: [LIBERAL_PARTY], fr: [LIBERAL_PARTY_LEGEND_FR] },
+  [NDP_PARTY_LEGEND]: { delta: 105, code: [NDP_PARTY], fr: [NDP_PARTY_LEGEND_FR] },
+};
+
+const ALT_COLOR_SCHEME = "zodiac_alt_colors";
+
 // ----------
+
+function getColorMap() {
+  console.log(`TRACER gCM cp 0`);
+
+  if (isAltColorScheme()) {
+    console.log(`TRACER gCM alt cp 1`);
+    return PARTY_ALT_COLOR_MAP;
+  } else {
+    return PARTY_COLOR_MAP;
+  }
+}
+
+function isAltColorScheme() {
+  let result = false;
+  if (sessionStorage.getItem(ALT_COLOR_SCHEME)) {
+    let value = sessionStorage.getItem(ALT_COLOR_SCHEME);
+    result = value === "true";
+  }
+  return result;
+}
+
+function setAltColorScheme(value) {
+  console.log(`TRACER sACS cp 1`);
+  sessionStorage.setItem(ALT_COLOR_SCHEME, "true");
+}
+
+function clearAltColorScheme(value) {
+  sessionStorage.removeItem(ALT_COLOR_SCHEME);
+}
+
+function getTextForLegend(legend) {
+  if (isFrenchMode()) {
+    return LEGEND_MAP[legend].fr;
+  } else {
+    return legend;
+  }
+}
+
+function getDeltaForLegend(legend) {
+  return LEGEND_MAP[legend].delta;
+}
+
+function getColorForLegend(legend) {
+  const colorMap = getColorMap();
+  return colorMap[LEGEND_MAP[legend].code];
+}
+
+function isFrenchMode() {
+  const fileName = location.href.split("/").slice(-1);
+  const isFrench = fileName == "index_fr.html";
+  return isFrench;
+}
 
 function getLocalizedJsonFile(jsonFile) {
   let result = jsonFile;
-  const fileName = location.href.split("/").slice(-1);
-  const isFrench = fileName == "index_fr.html";
-  if (isFrench) {
+  if (isFrenchMode()) {
     switch (jsonFile) {
       case NORMAL_JSON_FILE:
         result = NORMAL_JSON_FR_FILE;
@@ -90,7 +176,8 @@ function getFillColor(d) {
     }
   } else {
     // leaf
-    result = PARTY_COLOR_MAP[d.data.party];
+    const colorMap = getColorMap();
+    result = colorMap[d.data.party];
   }
   return result;
 }
@@ -126,10 +213,124 @@ function getTextClass(d) {
 // ----------
 
 function updateNormalMode() {
+  checkAltColorsForUI();
   drawCircle(NORMAL_JSON_FILE);
 }
 
+/*
+function drawLegend() {
+  let svg = d3.select("#legend");
+  svg.append("rect").attr("height", "100%").attr("width", "100%").attr("fill", d3.color(BACKGROUND_LIGHT));
+
+  let keys = [
+    GREEN_PARTY_LEGEND,
+    LIBERAL_PARTY_LEGEND,
+    NDP_PARTY_LEGEND,
+    CONSERVATIVE_PARTY_LEGEND,
+    BLOC_QUEBECOIS_PARTY_LEGEND,
+    INDEPENDENT_LEGEND,
+  ];
+
+  const firstDotX = 20;
+  const firstDotY = 20;
+  const deltaY = 25;
+  const radius = 7;
+
+  svg
+    .selectAll("mydots")
+    .data(keys)
+    .enter()
+    .append("circle")
+    .attr("cx", firstDotX)
+    .attr("cy", function (d, i) {
+      return firstDotY + i * deltaY;
+    }) // 100 is where the first dot appears. 25 is the distance between dots
+    .attr("r", radius)
+    .style("fill", function (d) {
+      return getColorForLegend(d);
+    });
+
+  const firstLabelX = firstDotX + 20;
+  const firstLabelY = firstDotY + 10;
+
+  svg
+    .selectAll("mylabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", firstLabelX)
+    .attr("y", function (d, i) {
+      return firstLabelY + i * deltaY;
+    })
+    .style("fill", function (d) {
+      return getColorForLegend(d);
+    })
+    .text(function (d) {
+      return d;
+    })
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle");
+}
+*/
+
+function drawHorizontalLegend() {
+  let svg = d3.select("#legend");
+  svg.append("rect").attr("height", "100%").attr("width", "100%").attr("fill", d3.color(BACKGROUND_LIGHT));
+
+  let keys = [
+    GREEN_PARTY_LEGEND,
+    LIBERAL_PARTY_LEGEND,
+    NDP_PARTY_LEGEND,
+    CONSERVATIVE_PARTY_LEGEND,
+    BLOC_QUEBECOIS_PARTY_LEGEND,
+    INDEPENDENT_LEGEND,
+  ];
+
+  const firstDotX = 20;
+  const firstDotY = 20;
+  // const delta = 80;
+  const radius = 7;
+
+  svg
+    .selectAll("mydots")
+    .data(keys)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d, i) {
+      return firstDotX + i * getDeltaForLegend(d);
+    })
+    .attr("cy", firstDotY)
+    .attr("r", radius)
+    .style("fill", function (d) {
+      return getColorForLegend(d);
+    });
+
+  const firstLabelX = firstDotX + 20;
+  const firstLabelY = firstDotY + 7;
+
+  svg
+    .selectAll("mylabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", function (d, i) {
+      return firstLabelX + i * getDeltaForLegend(d);
+    })
+    .attr("y", firstLabelY)
+    .style("fill", function (d) {
+      return getColorForLegend(d);
+    })
+    .text(function (d) {
+      return getTextForLegend(d);
+    })
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle");
+}
+
 function drawCircle(jsonFile) {
+  // const circleWidth = 660;
+  // const diameter = circleWidth;
+  // const translateX = diameter / 2;
   let svg = d3.select("#known"),
     margin = 20,
     diameter = +svg.attr("width"),
@@ -191,6 +392,8 @@ function drawCircle(jsonFile) {
         return d.data.name;
       });
 
+    drawHorizontalLegend();
+
     let node = g.selectAll("circle,text");
 
     svg.style("background", color(-1)).on("click", function () {
@@ -243,15 +446,35 @@ function drawCircle(jsonFile) {
 
 // ----------------- DOM/event handlers
 
+let currentMode = MODE_NORMAL;
+
 function modeCheckboxHandler(event) {
   const mode = event.target.value;
+  currentMode = mode;
   drawCircle(MODE_MAP[mode]);
+}
+
+function checkAltColorsForUI() {
+  const value = isAltColorScheme();
+  let cb = document.getElementById("checkbox-alt-colors");
+  cb.checked = value;
+}
+
+function altColorsHandler(cb) {
+  if (cb.target.checked) {
+    console.log(`TRACER handler checked`);
+    setAltColorScheme();
+  } else {
+    clearAltColorScheme();
+  }
+  drawCircle(MODE_MAP[currentMode]);
 }
 
 document.getElementById("checkbox-normal").addEventListener("change", modeCheckboxHandler);
 document.getElementById("checkbox-elements").addEventListener("change", modeCheckboxHandler);
 document.getElementById("checkbox-provinces").addEventListener("change", modeCheckboxHandler);
 document.getElementById("checkbox-unknown").addEventListener("change", modeCheckboxHandler);
+document.getElementById("checkbox-alt-colors").addEventListener("click", altColorsHandler);
 
 const backdrop = document.querySelector(".backdrop");
 const toggleButton = document.querySelector(".toggle-button");
